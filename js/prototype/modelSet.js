d64bbe08ddc3f29bce30715faf2de6df863345d6
Miguel del Mazo <migueldelmazo@gmail.com>
2014-09-26 21:05:51 +0200

    Refactor in Model.set for use dot notation

--- js/prototype/modelSet.js
+++ js/prototype/modelSet.js
@@ -4,35 +4,43 @@ define(['utils'], function (utils) {
 
     var //parse value before set
         parseBeforeSet = function (attr, value) {
-            _.each(this._parseBeforeSet, function (parse, parseAttr) {
-                if (parseAttr === attr) {
-                    if (parse.parse === 'parseInt') {
+            if (_.isString(attr)) {
+                _.each(this._parseBeforeSet, function (parse, parseAttr) {
+                    if (parseAttr === attr && parse.parse === 'parseInt') {
                         value = utils.parseInt(value, parse.defaultValue);
                     }
-                }
-            }, this);
+                });
+            }
             return value;
         },
 
-        //check if attr is in _validateBeforeSet and valida
+        //check if attr is in _validateBeforeSet and validate
         validateBeforeSet = function (attr, value) {
-            if (_.isString(attr) && utils.parseArray(this._validateBeforeSet).indexOf(attr) >= 0) {
-                return this.validate(attr, value);
-            } else {
-                return true;
-            }
+            return utils.parseArray(this._validateBeforeSet).indexOf(attr) >= 0 ? this.validate(attr, value): true;
         },
 
-        constructorModel = Backbone.Model;
+        //set in model with dot notation
+        setDotNot = function (backboneSetMethod, dotNotKeys, value) {
+            backboneSetMethod.call(this, utils.setDotNot(this.toJSON(), dotNotKeys, value), value);
+            this.trigger('change');
+        };
 
-    //overwrite set method
+    //wrap set method
 
-    Backbone.Model.prototype.set = _.wrap(Backbone.Model.prototype.set, function (setFunc, attr, value, options) {
-        if (validateBeforeSet.call(this, attr, value)) {
-            value = _.isString(attr) ? parseBeforeSet.call(this, attr, value) : value; //parse value
-            setFunc.call(this, attr, value, options); //Backbone.Model set
+    Backbone.Model.prototype.set = _.wrap(Backbone.Model.prototype.set, function (backboneSetMethod, attr, value, options) {
+        if (_.isString(attr)) {
+            if (validateBeforeSet.call(this, attr, value)) {
+                value = parseBeforeSet.call(this, attr, value);
+                if (attr.indexOf('.') >= 0) {
+                    setDotNot.call(this, backboneSetMethod, attr, value);
+                } else {
+                    backboneSetMethod.call(this, attr, value, options);
+                }
+            }
+            //if value is equal than previous value, Backbone doesn't trigger change:attr. We always trigger set:attr
+            this.trigger('set:' + attr);
+        } else {
+            backboneSetMethod.call(this, attr, value); //if attr is an object, call method normally
         }
-        //if value is equal than previous value, Backbone doesn't trigger change:attr. We always trigger set:attr
-        this.trigger('set:' + attr);
     });
 });
