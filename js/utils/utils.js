1b61b5797270bbba86bdf92640599e9c7228e44f
Miguel del Mazo <migueldelmazo@gmail.com>
2014-09-26 21:02:51 +0200

    New utils method setDotNot: set value in object with dot notation

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -54,18 +54,46 @@ define([
                 return JSON.parse(JSON.stringify(obj));
             },
 
-            //return an attribute of an object with dot notation
-
+            //return the last child of an object with dot notation
             dotNotObj: function (obj, keys) {
                 return _.reduce(keys.split('.'), function (memo, key) {
                     return _.isObject(memo[key]) ? memo[key] : memo;
                 }, obj);
             },
 
+            //return an attribute of an object with dot notation
             dotNot: function (obj, keys) {
                 return _.reduce(keys.split('.'), function (memo, key) {
                     return memo && memo[key] || undefined;
                 }, obj);
+            },
+
+            //set value in object with dot notation
+            setDotNot: function (dest, keys, value) {
+                var setDotNotValue = function (parts, value) {
+                        var obj = {},
+                            child = {},
+                            part;
+
+                        if (_.isString(parts)) { //split parts first time
+                            parts = parts.split('.');
+                        }
+                        part = parts[0];
+
+                        if (_.size(parts) === 1) { //this is the last child, set value
+                            obj[part] = value;
+                        } else {
+                            child = setDotNotValue(parts.slice(1), value); //recursively call
+                            if (!isNaN(part)) {
+                                obj = []; //is an array
+                                obj[parseInt(part, 10)] = child;
+                            } else {
+                                obj[part] = child; //is an object
+                            }
+                        }
+                        return obj;
+                    };
+                return $.extend(true, dest, setDotNotValue(keys, value));
             },
 
             //call a function or return an attribute of an object, with dot notation
